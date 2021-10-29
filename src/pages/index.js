696d319bb631191b9cbe718a7f17f4cfe641cd41
Ayan <ayan@elsewhen.com>
2021-10-29 16:06:26 +0100

    reset gatsby website without algolia

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,9 @@
 import * as React from "react"
-import { graphql } from "gatsby"
-import algoliasearch from "algoliasearch/lite"
-import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom"
+import { Link, graphql } from "gatsby"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import PostPreview from "../components/post-preview"
-
-const searchClient = algoliasearch(
-  "AQ5OSHFUGN",
-  "ba9b05ceff50f110219fa1bbbfd7276e"
-)
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
@@ -35,10 +27,38 @@ const BlogIndex = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <Seo title="All posts" />
       <Bio />
-      <InstantSearch searchClient={searchClient} indexName="Gatsby blog">
-        <SearchBox />
-        <Hits hitComponent={PostPreview} />
-      </InstantSearch>
+      <ol style={{ listStyle: `none` }}>
+        {posts.map(post => {
+          const title = post.frontmatter.title || post.fields.slug
+
+          return (
+            <li key={post.fields.slug}>
+              <article
+                className="post-list-item"
+                itemScope
+                itemType="http://schema.org/Article"
+              >
+                <header>
+                  <h2>
+                    <Link to={post.fields.slug} itemProp="url">
+                      <span itemProp="headline">{title}</span>
+                    </Link>
+                  </h2>
+                  <small>{post.frontmatter.date}</small>
+                </header>
+                <section>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: post.frontmatter.description || post.excerpt,
+                    }}
+                    itemProp="description"
+                  />
+                </section>
+              </article>
+            </li>
+          )
+        })}
+      </ol>
     </Layout>
   )
 }
